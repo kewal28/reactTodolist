0471cf10d86192a8efb17b2861259f716d1a0e01
Kewal Kanojia <kewalkanojia@admins-MacBook-Pro.local>
2019-08-06 23:27:27 +0530

    Base Code Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View><Text>Hello World!!</Text></View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
